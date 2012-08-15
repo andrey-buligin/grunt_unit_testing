ebfc6c42fb6efa47cc863f34e9cb5459a472246d
Andrey Buligin <andrey.buligin@lbi.com>
2012-08-15 10:06:39 +0100

    Initial commit

---
+++ src/grunt_test.js
@@ -0,0 +1,28 @@
+/*
+ * grunt_test
+ * 
+ *
+ * Copyright (c) 2012 Andrey Buligin
+ * Licensed under the MIT, GPL licenses.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
